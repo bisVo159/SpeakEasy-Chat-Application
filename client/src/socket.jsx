d329e8f3bb2e9f238f44765f662b300e9e701581
bisVo159 <biswasanik159@gmail.com>
2024-09-12 18:38:35 +0530

    env variable change

--- client/src/socket.jsx
+++ client/src/socket.jsx
@@ -1,5 +1,6 @@
 import { createContext, useContext, useMemo } from "react"
 import io from "socket.io-client"
+export const server=import.meta.env.server;
 
 const SocketContext=createContext();
 
@@ -7,7 +8,7 @@ const getSocket=()=>useContext(SocketContext)
 
 const SocketProvider=({children})=>{
 
-    const socket=useMemo(()=>io(process.env.server,{withCredentials:true}),[])
+    const socket=useMemo(()=>io(server,{withCredentials:true}),[])
 
     return(
         <SocketContext.Provider value={socket}>
